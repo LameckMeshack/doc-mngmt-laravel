f5d4e8c2727471ed2c45106fdb4e1afa7e2f62db
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-27 16:06:50 +0300

    *Refactored and ran migrations for all model

--- database/migrations/2022_09_27_094235_create_documents_table.php
+++ database/migrations/2022_09_27_094235_create_documents_table.php
@@ -14,8 +14,17 @@ class CreateDocumentsTable extends Migration
     public function up()
     {
         Schema::create('documents', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('path');
+            $table->text('description');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('department_id');
+            $table->unsignedInteger('type_id');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
