270928bf3c4df9cb1da6dbab2fd00c2f10d271d9
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-27 21:36:22 +0300

    *Added Routes API for documents

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//get all document
+Route::get('documents', [DocumentController::class, 'index']);
+
+//get single document
+Route::get('documents/{id}', [DocumentController::class, 'show']);
+
+//post/ create document api
+Route::post('documents', [DocumentController::class, 'store']);
+
+// update  document api
+Route::put('documents/{id}', [DocumentController::class, 'update']);
+
+// delete document api
+Route::delete('documents/{id}', [DocumentController::class, 'destroy']);
