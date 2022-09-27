f697d44f65c0162734eebf640752c00b5d252e53
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-27 19:37:26 +0300

    *Added User repository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\userRepositoryInterface;
+use App\Models\Order;
+use App\Models\User;
+
+class OrderRepository implements userRepositoryInterface
+{
+
+
+
+
+    public function getAllUsers()
+    {
+        return User::all();
+    }
+
+    public function getUserById($userId)
+    {
+        return User::find($userId);
+    }
+
+    public function deleteUser($userId)
+    {
+        return User::destroy($userId);
+    }
+
+    public function createUser(array $userDetails)
+    {
+        return User::create($userDetails);
+    }
+
+    public function updateUser($userId, array $newDetails)
+    {
+        $user = User::find($userId);
+        return $user->update($newDetails);
+    }
+}
