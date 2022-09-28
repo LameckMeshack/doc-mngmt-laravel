8a49f5949687807ddd39706deeb8bfd5f0a9577d
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-28 11:11:14 +0300

    *Refactored department controller by addding functionalities

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -2,20 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\departmentRepositoryInterface;
 use App\Models\Department;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+
 
 class DepartmentController extends Controller
 {
+    // constructor
+    public function __construct(departmentRepositoryInterface $departmantRepository)
+    {
+        $this->departmentRepository = $departmantRepository;
+        // $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): JsonResponse
     {
         //
+        $departments = $this->departmentRepository->getAllDepartments();
+        return response()->json($departments);
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -33,9 +45,14 @@ class DepartmentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         //
+        $departmentDetails = $request->only([
+            "name",
+        ]);
+        $department = $this->departmentRepository->createDepartment($departmentDetails);
+        return response()->json($department);
     }
 
     /**
@@ -44,10 +61,14 @@ class DepartmentController extends Controller
      * @param  \App\Models\Department  $department
      * @return \Illuminate\Http\Response
      */
-    public function show(Department $department)
+    public function show(Request $request): JsonResponse
     {
         //
+        $departmentId = $request->route('id');
+        $department = $this->departmentRepository->getDepartmentById($departmentId);
+        return response()->json($department);
     }
+
 
     /**
      * Show the form for editing the specified resource.
@@ -67,9 +88,15 @@ class DepartmentController extends Controller
      * @param  \App\Models\Department  $department
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Department $department)
+    public function update(Request $request)
     {
         //
+        $departmentDetails = $request->only([
+            "name",
+        ]);
+        $departmentId = $request->route('id');
+        $department = $this->departmentRepository->updateDepartment($departmentId, $departmentDetails);
+        return response()->json($department);
     }
 
     /**
@@ -78,8 +105,13 @@ class DepartmentController extends Controller
      * @param  \App\Models\Department  $department
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Department $department)
+    public function destroy(Request $request, Department $department)
     {
         //
+        $departmentId = $request->only([
+            "id",
+        ]);
+        $department = $this->departmentRepository->deleteDepartment($departmentId);
+        return response()->json($department);
     }
 }
