7f3af21eb7364f9f063be37b312f3f3cc3fea99a
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-28 10:10:03 +0300

    *Added API routes for types

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// DOCUMENT ROUTES
 //get all document
 Route::get('documents', [DocumentController::class, 'index']);
 
@@ -33,3 +35,22 @@ Route::put('documents/{id}', [DocumentController::class, 'update']);
 
 // delete document api
 Route::delete('documents/{id}', [DocumentController::class, 'destroy']);
+
+// TYPE ROUTES
+
+//get all types
+Route::get('types', [TypeController::class, 'index']);
+
+//get single type
+Route::get('types/{id}', [TypeController::class, 'show']);
+
+//post/ create type api
+Route::post('types', [TypeController::class, 'store']);
+
+// update  type api
+Route::put('types/{id}', [TypeController::class, 'update']);
+
+// delete type api
+Route::delete('types/{id}', [TypeController::class, 'destroy']);
+
+// DEPARTMENT ROUTES
