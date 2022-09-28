40d0badcce606fa94acd69be3d639c2f2b5fa4da
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-28 23:08:13 +0300

    *added Headr

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Login from "./Login";
 
 const Header = () => (
-    <nav className="navbar navbar-expand-md navbar-light navbar-laravel">
-        <div className="container">
-            <Link className="navbar-brand" to="/">
-                Document management
-            </Link>
-            <Login />
-        </div>
-    </nav>
+    <>
+        <nav className="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div className="container">
+                <Link className="navbar-brand" to="/">
+                    Document management
+                </Link>
+                <Link to="login">Login</Link>
+                <Link to="register">Register</Link>
+            </div>
+        </nav>
+    </>
 );
 
 export default Header;
