e43f7ec4854e266c40ae94761105822763bd882d
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-29 16:56:35 +0300

    *Refactored web.php to add a wild card for react

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('app');
-});
+// Route::get('/', function () {
+//     return view('app');
+// });
 
 // Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // //User Profil
 // Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+Route::view('/{path?}', 'app')
+    ->where('path', '.*');
