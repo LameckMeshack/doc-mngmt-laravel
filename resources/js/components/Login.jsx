4168daa6af2c96e2b9e6c4af2a3224bec75e37f1
RhinolamerSonwar <rhinolamer@gmail.com>
2022-09-29 16:57:33 +0300

    *refatored Login

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -1,17 +1,71 @@
 import axios from "axios";
+import { useState } from "react";
 
 function Login() {
-    // fetch data using axios
+    const [loginData, setloginData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        axios
+            .post("/api/login", loginData)
+            .then((res) => {
+                console.log(res);
+                // console.log(loginData);
+            })
+            .catch((err) => {
+                setError(err.response.data.message);
+                console.log(error);
+            });
+    };
+
+    if (error) {
+        alert(error);
+    }
+
+    //success message if login successful
 
     return (
-        <div className="center-item">
-            <form>
-                <input type="text" placeholder="Enter username" />
-                <input type="password" placeholder="Enter password" />
-                <button type="button">Login</button>
-                <p>
-                    Not Registered ?<a href="#">Create account Now!</a>
-                </p>
+        <div className="login">
+            <form onSubmit={handleLogin} className="">
+                <div className="form-group">
+                    <label htmlFor="email" required>
+                        Email
+                    </label>
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="name"
+                        onChange={(e) =>
+                            setloginData({
+                                ...loginData,
+                                email: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label> <br />
+                    <input
+                        type="password"
+                        required
+                        placeholder="password"
+                        // setOnchange
+                        onChange={(e) =>
+                            setloginData({
+                                ...loginData,
+                                password: e.target.value,
+                            })
+                        }
+                    />
+                </div>
+                <button type="submit" className="btn ">
+                    Login
+                </button>
             </form>
         </div>
     );
