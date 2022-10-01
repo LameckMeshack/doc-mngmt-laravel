b12830c204304cf6631b561bbb880eecddf1025e
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-01 20:29:16 +0300

    *Refactored the store method 	*Implemented validation of the request 	*Implemeted file handling and changing the name of the file to the time it has been uploaded 	*Saving the uploaded file functionality

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -8,6 +8,7 @@ use App\Interfaces\documentRepositoryInterface;
 use App\Models\Document;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 class DocumentController extends Controller
 {
@@ -53,16 +54,51 @@ class DocumentController extends Controller
     public function store(Request $request)
     {
         //
-        $documenDetails = $request->only([
+
+        // dd($request->all());
+        // validate the request
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'access' => 'required|string',
+            'department_id' => 'required|integer',
+            'type_id' => 'required|integer',
+            'user_id' => 'required|integer',
+            'file' => 'required|mimes:pdf,doc,docx,txt,pdf,zip,rar,ppsx,pptx,ppt,odt,ods,odp,odg,odf,odc,odb,odm,rtf,html,htm,xml,xls,xlsx, csv,tsv',
+        ]);
+        // if request validation fails
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validate->errors(),
+            ], 400);
+        }
+
+
+
+        //    handle the file uploads
+        if ($request->hasFile('file')) {
+            $file_name = $request->file('file');
+            $name = time() . '.' . $file_name->getClientOriginalExtension();
+            $destinationPath = public_path('/uploads/documents');
+            $file_name->move(
+                $destinationPath,
+                $name
+            );
+        }
+
+        $documentDetails = $request->only([
             "name",
-            "path",
             "description",
-            "user_id",
-            "department_id",
+            'access',
             "type_id",
+            "department_id",
+            "user_id",
+            'file',
         ]);
 
-        $document = $this->documentRepository->createDocument($documenDetails);
+        $documentDetails['file'] = $name;
+        $document = $this->documentRepository->createDocument($documentDetails);
         return response()->json($document);
     }
 
@@ -110,8 +146,9 @@ class DocumentController extends Controller
         $documentId = $request->route('id');
         $documentDetails = $request->only([
             "name",
-            "path",
+            "access",
             "description",
+            'file',
             "user_id",
             "department_id",
             "type_id",
