8fedcd13ef32fb411e1905534692a5f9401ffd07
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-02 22:27:29 +0300

    *Created document constants

---
+++ resources/js/store/constants/documentConstants.js
@@ -0,0 +1,6 @@
+//document constants
+export const GET_DOCUMENTS = "GET_DOCUMENTS";
+export const GET_DOCUMENT = "GET_DOCUMENT";
+export const CREATE_DOCUMENT = "CREATE_DOCUMENT";
+export const UPDATE_DOCUMENT = "UPDATE_DOCUMENT";
+export const DELETE_DOCUMENT = "DELETE_DOCUMENT";
