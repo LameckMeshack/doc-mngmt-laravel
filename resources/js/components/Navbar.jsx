a1ced515c0e736b0d56f7eb50c9373746bf36a31
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 16:12:11 +0300

    *Refactored navbar 	#Added conditional rendering of the links

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { logout } from "../store/actions/authActions";
 
 export default function Navbar() {
+    const dispatch = useDispatch();
     const [isNavExpanded, setIsNavExpanded] = useState(false);
+    //get user from the context
+    const { user } = useContext(AuthContext);
+
+    console.log(user?.user);
 
     return (
         <nav className="navigation">
@@ -24,20 +32,29 @@ export default function Navbar() {
                         : "navigation-menu"
                 }
             >
-                <ul>
-                    <li>
-                        <NavLink to="home">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="login">Login</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="register">Register</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="upload">Upload</NavLink>
-                    </li>
-                </ul>
+                {/* if user exists, show the following links else show the login links */}
+                {user?.user ? (
+                    <ul>
+                        <li>
+                            <NavLink to="documents">Documents</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="departments">Departments</NavLink>
+                        </li>
+                        <li>
+                            <NavLink onClick={dispatch(logout)}>Logout</NavLink>
+                        </li>
+                    </ul>
+                ) : (
+                    <ul>
+                        <li>
+                            <NavLink to="login">Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="register">Register</NavLink>
+                        </li>
+                    </ul>
+                )}
             </div>
         </nav>
     );
