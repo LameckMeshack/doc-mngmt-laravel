d2340994e3b398053a5833c677515e5164d6c815
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 21:43:44 +0300

    *Added department reducer to the root reducer

--- resources/js/store/reducers/index.js
+++ resources/js/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducers";
+import departmentReducer from "./departmentReducers";
 import documentReducer from "./documentReducers";
 
 //combining reducers
 export const rootReducer = combineReducers({
     userInfo: authReducer,
     documents: documentReducer,
+    departments: departmentReducer,
 });
