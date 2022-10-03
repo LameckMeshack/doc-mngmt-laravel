9724d1b24550782146554eecddeb98bc77f9b2c0
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 13:30:47 +0300

    *Added props to the document card

--- resources/js/components/DocumentCard.jsx
+++ resources/js/components/DocumentCard.jsx
@@ -1,14 +1,14 @@
 import { FcFile } from "react-icons/fc";
-function DocumentCard() {
+function DocumentCard({ name, access, department }) {
     return (
         <div className="document-card">
             <div className="document-card-left">
                 <FcFile className="document-card-left-icon" />
             </div>
             <div className="document-card-right">
-                <h5>Document Name</h5>
-                <small>Document Acess</small>
-                <small> department</small>
+                <h5>{name}</h5>
+                <small>{access}</small>
+                <small> {department}</small>
             </div>
         </div>
     );
