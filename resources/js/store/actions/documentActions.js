66747f48062183fa85ea8a6ef592c01c37f5519a
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 09:27:56 +0300

    *Added Actions 	#Added action for creating the document 	#Added action for getting all the documents

---
+++ resources/js/store/actions/documentActions.js
@@ -0,0 +1,63 @@
+import axios from " axios ";
+import {
+    GET_DOCUMENTS,
+    GET_DOCUMENT,
+    CREATE_DOCUMENT,
+    UPDATE_DOCUMENT,
+    DELETE_DOCUMENT,
+    DOCACTIONFAILED,
+} from "../constants/documentConstants";
+
+export const createDocument = (documentDetails) => async (dispatch) => {
+    try {
+        const { data } = await axios.post("/api/documents", documentDetails);
+        dispatch({
+            type: CREATE_DOCUMENT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DOCACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const getDocuments = () => async (dispatch) => {
+    try {
+        const { data } = await axios.get("/api/documents");
+        dispatch({
+            type: GET_DOCUMENTS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DOCACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const getDocument = (id) => async (dispatch) => {
+    try {
+        const { data } = await axios.get(`/api/documents/${id}`);
+        dispatch({
+            type: GET_DOCUMENT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DOCACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
