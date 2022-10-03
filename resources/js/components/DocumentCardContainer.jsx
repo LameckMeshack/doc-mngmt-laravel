1ec1c7ba69ec8880e662b6de9523680b4426fffa
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 13:31:06 +0300

    *Created document cards holder

---
+++ resources/js/components/DocumentCardContainer.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDocuments } from "../store/actions/documentActions";
+import DocumentCard from "./DocumentCard";
+
+function DocumentCardContainer() {
+    const { documents } = useSelector((state) => state.documents);
+    console.log(documents);
+
+    const dispacth = useDispatch();
+    useEffect(() => {
+        dispacth(getDocuments());
+    }, [dispacth]);
+    return (
+        <div className="document-card-container">
+            <h1>Document Card Container</h1>
+            <div className="cards-container">
+                {documents.map((document) => (
+                    <DocumentCard
+                        key={document.id}
+                        name={document.name}
+                        access={document.access}
+                        department={document.department.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default DocumentCardContainer;
