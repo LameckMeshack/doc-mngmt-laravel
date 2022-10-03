547275fc35a9a6dc4ed2aa0969261936a4a96ae3
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 23:03:56 +0300

    *Refactored document

--- resources/js/components/SpecificDeptCardContainer.jsx
+++ resources/js/components/SpecificDeptCardContainer.jsx
@@ -13,6 +13,14 @@ function SpecificDeptCardContainer() {
         (document) => document.department.id === parseInt(departmentId)
     );
 
+    // get the department name through the department id in the url
+    // const { departments } = useSelector((state) => state.departments);
+    // const department = departments.find(
+    //     (department) => department.id === parseInt(departmentId)
+    // );
+    // console.log(department);
+    // const departmentName = department.id;
+
     const dispacth = useDispatch();
     useEffect(() => {
         dispacth(getDocuments());
@@ -21,18 +29,18 @@ function SpecificDeptCardContainer() {
         //    if filterfDocuments is empty return a message else return the filtered documents
         <div className="container">
             {filteredDocuments.length === 0 ? (
-                <h1>No documents found</h1>
+                <h1>No documents found </h1>
             ) : (
                 <div>
                     <div className="document-card-container">
-                        <h1>Files</h1>
+                        <h1>Files </h1>
                         <div className="cards-container">
-                            {filteredDocuments.map((document) => (
+                            {filteredDocuments.map((doc) => (
                                 <DocumentCard
-                                    key={document.id}
-                                    name={document.name}
-                                    access={document.access}
-                                    department={document.department.name}
+                                    key={doc.id}
+                                    name={doc.name}
+                                    access={doc.access}
+                                    department={doc.department.name}
                                 />
                             ))}
                         </div>
