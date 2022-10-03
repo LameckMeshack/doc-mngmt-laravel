9d788d49754e375635ce5aecf4126b55c4a309d1
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 21:37:37 +0300

    *Refactored action 	#Added action for update, delete, create and getting departments

--- resources/js/store/actions/departmentActions.js
+++ resources/js/store/actions/departmentActions.js
@@ -1,25 +1,105 @@
 import axios from "axios";
-
 import {
     GET_DEPARTMENTS,
-    ADD_DEPARTMENT,
+    GET_DEPARTMENT,
+    CREATE_DEPARTMENT,
+    UPDATE_DEPARTMENT,
     DELETE_DEPARTMENT,
-    DEPARTMENTS_LOADING,
-} from "./types";
+    DEPACTIONFAILED,
+} from "../constants/departmentConstants";
 
-export const getDepartments = () => (dispatch) => {
-    axios
-        .get("/api/departments")
-        .then((res) =>
-            dispatch({
-                type: GET_DEPARTMENTS,
-                payload: res.data,
-            })
-        )
-        .catch((err) =>
-            dispatch({
-                type: GET_DEPARTMENTS,
-                payload: null,
-            })
+export const createDepartment = (departmentDetails) => async (dispatch) => {
+    try {
+        const { data } = await axios.post(
+            "/api/departments",
+            departmentDetails
         );
+        dispatch({
+            type: CREATE_DEPARTMENT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DEPACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const getDepartments = () => async (dispatch) => {
+    try {
+        const { data } = await axios.get("/api/departments");
+        dispatch({
+            type: GET_DEPARTMENTS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DEPACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const getDepartment = (id) => async (dispatch) => {
+    try {
+        const { data } = await axios.get(`/api/departments/${id}`);
+        dispatch({
+            type: GET_DEPARTMENT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DEPACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const updateDepartment = (id, departmentDetails) => async (dispatch) => {
+    try {
+        const { data } = await axios.put(
+            `/api/departments/${id}`,
+            departmentDetails
+        );
+        dispatch({
+            type: UPDATE_DEPARTMENT,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: DEPACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const deleteDepartment = (id) => async (dispatch) => {
+    try {
+        await axios.delete(`/api/departments/${id}`);
+        dispatch({
+            type: DELETE_DEPARTMENT,
+            payload: id,
+        });
+    } catch (error) {
+        dispatch({
+            type: DEPACTIONFAILED,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
 };
