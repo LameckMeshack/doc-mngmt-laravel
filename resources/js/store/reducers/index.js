9542dbd60bed229ffe0c5fdf421683755bfcd018
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 09:48:05 +0300

    *Imported documennt Reducers 	#Assigned document reducer to document in the rootReducer

--- resources/js/store/reducers/index.js
+++ resources/js/store/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducers";
+import documentReducer from "./documentReducers";
 
 //combining reducers
 export const rootReducer = combineReducers({
     userInfo: authReducer,
+    documents: documentReducer,
 });
