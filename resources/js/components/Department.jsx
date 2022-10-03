f9c5ac7f4a47d8eae442accf75205c6e0317475b
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-03 22:00:08 +0300

    *Refactored depertment 	#Displaying value from the store 	#Added props to child cards 	#added Hyperlink to the cards

--- resources/js/components/Department.jsx
+++ resources/js/components/Department.jsx
@@ -1,24 +1,32 @@
+import { useEffect } from "react";
 import { FcOpenedFolder } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getDepartments } from "../store/actions/departmentActions";
 
 function Departments() {
+    const departments = useSelector((state) => state.departments.departments);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getDepartments());
+    }, []);
+
     return (
         <div className="departments">
             <h1>Available Departments</h1>
             <div className="departments-container">
-                <div className="department-folder">
-                    <FcOpenedFolder className="department-folder-icon" />
-                    <h5>Department</h5>
-                </div>
-
-                <div className="department-folder">
-                    <FcOpenedFolder className="department-folder-icon" />
-                    <h5>Department</h5>
-                </div>
-
-                <div className="department-folder">
-                    <FcOpenedFolder className="department-folder-icon" />
-                    <h5>Department</h5>
-                </div>
+                {departments.map((department) => (
+                    <Link
+                        to={`/department/${department.id}`}
+                        key={department.id}
+                    >
+                        <div key={department.id} className="department-folder">
+                            <FcOpenedFolder className="department-folder-icon" />
+                            <h5>{department.name}</h5>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </div>
     );
